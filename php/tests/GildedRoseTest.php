a24f3b74f7738c610fdfcda4564ee16b1d20b439
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 11:16:44 +0200

    :white_check_mark: Added unit tests for 'Aged Brie'

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,36 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testAgedBrieBeforeSellInDate(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('Aged Brie', 10, 10)];
         $gildedRose = new GildedRose($items);
+
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+
+        $this->assertEquals(9, $items[0]->sell_in);
+        $this->assertEquals(11, $items[0]->quality);
+    }
+
+    public function testAgedBrieOnSellInDate(): void
+    {
+        $items = [new Item('Aged Brie', 0, 10)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(12, $items[0]->quality);
+    }
+
+    public function testAgedBrieAfterSellInDate(): void
+    {
+        $items = [new Item('Aged Brie', -5, 10)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(-6, $items[0]->sell_in);
+        $this->assertEquals(12, $items[0]->quality);
     }
 }
