6f03715596d829a9d34fd354285ca5dfb673394a
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 12:58:20 +0200

    :recycle: Sulfuras is now extending AbstractInventoryModel and implemented empty dayPasses

--- php/src/Inventory/Model/Sulfuras.php
+++ php/src/Inventory/Model/Sulfuras.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose\Inventory\Model;
 
-class Sulfuras
+class Sulfuras extends AbstractInventoryModel
 {
     public const NAME = 'Sulfuras, Hand of Ragnaros';
 
@@ -12,5 +12,10 @@ class Sulfuras
     {
         // TODO: implement logic that uses a Legendary quality property that Sulfuras is supposed to have
         //  i.e. 80 quality and doesn't increase/decrease also no sellIn date
+    }
+
+    public function dayPasses(): void
+    {
+        // Sulfuras shouldn't use the dayPasses function since it doesn't do anything
     }
 }
