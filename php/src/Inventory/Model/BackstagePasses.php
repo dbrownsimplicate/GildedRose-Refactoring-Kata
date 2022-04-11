5f2a0a8af32ba0b4afa4364d2faa29124d174ce5
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 13:21:49 +0200

    :recycle: Updated BackstagePasses dayPasses logic to include all cases

--- php/src/Inventory/Model/BackstagePasses.php
+++ php/src/Inventory/Model/BackstagePasses.php
@@ -11,14 +11,27 @@ class BackstagePasses extends AbstractInventoryModel
     public function dayPasses(): void
     {
         $this->sellIn->decrease();
-        if ($this->sellIn->hasPassed()) {
-            $this->quality->setToZero();
+
+        switch ($this->sellIn) {
+            case $this->sellIn->hasPassed():
+                $this->quality->setToZero();
+                break;
+            case $this->sellIn->getDaysInteger() <= 5:
+                $this->quality->increase(3);
+                break;
+            case $this->sellIn->getDaysInteger() <= 10:
+                $this->quality->increase(2);
+                break;
+            default:
+                $this->quality->increase(1);
         }
 
+        $this->updateItem();
+
         // TODO: Implement logic that sets the following:
-        //  - Quality increases by 1 if sellIn date is more than 10
-        //  - Quality increases by 2 if sellIn date is less than or equal to 10
-        //  - Quality increases by 3 if sellIn date is less than or equal to 5
+        //  + Quality increases by 1 if sellIn date is more than 10
+        //  + Quality increases by 2 if sellIn date is less than or equal to 10
+        //  + Quality increases by 3 if sellIn date is less than or equal to 5
         //  + Quality drops to zero after or on SellIn date
     }
 }
