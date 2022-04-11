9f5dca7db642ab2d24ce6ea462bd2105febc2d7f
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 13:07:26 +0200

    :recycle: DexterityVest now extending AbstractInventoryModel

--- php/src/Inventory/Model/DexterityVest.php
+++ php/src/Inventory/Model/DexterityVest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose\Inventory\Model;
 
-class DexterityVest
+class DexterityVest extends AbstractInventoryModel
 {
     public const NAME = '+5 Dexterity Vest';
 }
