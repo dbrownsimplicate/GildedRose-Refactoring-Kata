d11ebbdf5d1bdf01bbd2fcac1fb4eee6d0e85106
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 12:26:12 +0200

    :recycle: Added SellIn property

---
+++ php/src/Inventory/Property/SellIn.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose\Inventory\Property;
+
+class SellIn
+{
+    private int $days;
+
+    public function __construct(int $days)
+    {
+        $this->days = $days;
+    }
+
+    public function decrease(): void
+    {
+        $this->days--;
+    }
+
+    public function hasPassed(): bool
+    {
+        return 1 > $this->days;
+    }
+
+    public function getNumberOfDays(): int
+    {
+        return $this->days;
+    }
+}
