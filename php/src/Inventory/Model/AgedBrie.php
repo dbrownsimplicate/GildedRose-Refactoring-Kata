4f5a7fbe850542d50139c3d06f10576477981590
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 13:09:07 +0200

    :recycle: AgedBrie now extending AbstractInventoryModel

--- php/src/Inventory/Model/AgedBrie.php
+++ php/src/Inventory/Model/AgedBrie.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose\Inventory\Model;
 
-class AgedBrie
+class AgedBrie extends AbstractInventoryModel
 {
     public const NAME = 'Aged Brie';
     protected int $qualityDegradation = -1;
