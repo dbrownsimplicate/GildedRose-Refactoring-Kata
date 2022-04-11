f9c097e85be52b4278e8d77a31551e11d7cfcf5b
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 11:22:17 +0200

    :white_check_mark: Added more unit tests for 'Aged Brie' with or near maximum Quality

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -42,4 +42,48 @@ class GildedRoseTest extends TestCase
         $this->assertEquals(-6, $items[0]->sell_in);
         $this->assertEquals(12, $items[0]->quality);
     }
+
+    public function testAgedBrieBeforeSellInDateWithMaximumQuality(): void
+    {
+        $items = [new Item('Aged Brie', 10, 50)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(9, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testAgedBrieOnSellInDateWithMaximumQuality(): void
+    {
+        $items = [new Item('Aged Brie', 0, 50)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testAgedBrieOnSellInDateNearMaximumQuality(): void
+    {
+        $items = [new Item('Aged Brie', 0, 49)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testAgedBrieAfterSellInDateWithMaximumQuality(): void
+    {
+        $items = [new Item('Aged Brie', -10, 50)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(-11, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
 }
