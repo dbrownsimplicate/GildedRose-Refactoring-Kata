6bdd313817c7d8c334bf808da44ddfd7364b5c55
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 12:34:11 +0200

    :recycle: Added LegendaryQuality property in this case specifically for Sulfuras

---
+++ php/src/Inventory/Property/LegendaryQuality.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose\Inventory\Property;
+
+class LegendaryQuality
+{
+    public const LEGENDARY_QUALITY = 80;
+    protected int $quality = self::LEGENDARY_QUALITY;
+}
