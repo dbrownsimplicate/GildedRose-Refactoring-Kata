9b2f71a46ffd5cd34fd5a8900748e96b80d12e24
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 13:26:32 +0200

    :recycle: Added InventoryBuilder to build each Model based on their name to be used in GildedRose class

---
+++ php/src/Inventory/Builder/InventoryBuilder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GildedRose\Inventory\Builder;
+
+use GildedRose\Inventory\Model\AbstractInventoryModel;
+use GildedRose\Inventory\Model\AgedBrie;
+use GildedRose\Inventory\Model\BackstagePasses;
+use GildedRose\Inventory\Model\ConjuredManaCake;
+use GildedRose\Inventory\Model\DexterityVest;
+use GildedRose\Inventory\Model\Elixir;
+use GildedRose\Inventory\Model\Sulfuras;
+use GildedRose\Item as GoblinItem;
+use RuntimeException;
+
+class InventoryBuilder
+{
+    public function build(GoblinItem $item): AbstractInventoryModel
+    {
+        switch ($item->name) {
+            case AgedBrie::NAME:
+                return AgedBrie::build($item);
+            case BackstagePasses::NAME:
+                return BackstagePasses::build($item);
+            case ConjuredManaCake::NAME:
+                return ConjuredManaCake::build($item);
+            case DexterityVest::NAME:
+                return DexterityVest::build($item);
+            case Elixir::NAME:
+                return Elixir::build($item);
+            case Sulfuras::NAME:
+                return Sulfuras::build($item);
+            default:
+                throw new RuntimeException(sprintf('Item [%s] does not exist', $item->name));
+        }
+    }
+}
