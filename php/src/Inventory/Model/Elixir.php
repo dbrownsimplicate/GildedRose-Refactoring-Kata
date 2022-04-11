221ea2893f71e73d967902349ee3216032f1e48e
Darren Brown <darren.brown@simplicate.nl>
2022-04-11 13:07:09 +0200

    :recycle: Elixir now extending AbstractInventoryModel

--- php/src/Inventory/Model/Elixir.php
+++ php/src/Inventory/Model/Elixir.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose\Inventory\Model;
 
-class Elixir
+class Elixir extends AbstractInventoryModel
 {
     public const NAME = 'Elixir of the Mongoose';
 }
